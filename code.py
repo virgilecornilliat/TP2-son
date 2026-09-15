4e6a2473c216358b2fd3b5f3c2f36916dd1411bd
Virgile Cornilliat <virgile.cornilliat@etu.minesparis.psl.eu>
2026-09-15 15:17:05 +0200

    question 2.1

--- code.py
+++ code.py
@@ -3,3 +3,19 @@ import matplotlib.pyplot as plt
 from IPython.display import Audio
 from scipy.io import wavfile
 
+RATE = 44_100
+LA = 440
+DO = 523.25
+
+
+def display_signal(freq):
+    t=np.linspace(0,1,int(freq))
+    tab=np.sin(2*np.pi*freq*t)
+    plt.plot(t,tab)
+    plt.show()
+
+
+
+
+
+
